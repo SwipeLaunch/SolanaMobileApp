16a0bcac9cca943f3739f0f42a77374797359225
Jianian Zheng <jnz7276@gmail.com>
2025-08-04 18:58:35 +0100

    Fix: credential storage for supabase

--- android/app/src/main/java/com/anonymous/SolanaMobileApp/SupabaseConfig.kt
+++ android/app/src/main/java/com/anonymous/SolanaMobileApp/SupabaseConfig.kt
@@ -5,12 +5,9 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseConfig {
     
-    private const val SUPABASE_URL = "https://hhihnakkhihvfxyhuetq.supabase.co"
-    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoaWhuYWtraGlodmZ4eWh1ZXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4NzIxNDAsImV4cCI6MjA2OTQ0ODE0MH0.bUHPq-SXJtQnPKzbrvjYISij6_F40cfeS6eoaZIuyYI"
-    
     val client = createSupabaseClient(
-        supabaseUrl = SUPABASE_URL,
-        supabaseKey = SUPABASE_ANON_KEY
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY
     ) {
         install(Postgrest)
         // Note: Using default configuration for now due to emulator network issues
